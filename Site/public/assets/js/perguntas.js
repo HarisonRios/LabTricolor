261adcf4f2feff0f57686533bc9e8a7349add288
Harison Rios de Miranda <hharison562@gmail.com>
2024-11-30 15:24:32 -0300

    atualizando o quiz

---
+++ Site/public/assets/js/perguntas.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+      numb: 1,
+      question: "Em que ano o São Paulo Futebol Clube foi fundado?",
+      answer: "1930",
+      options: ["1922", "1930", "1935", "1940"],
+    },
+    {
+      numb: 2,
+      question: "Qual é o estádio do São Paulo FC?",
+      answer: "Morumbi",
+      options: ["Maracanã", "Arena Corinthians", "Morumbi", "Pacaembu"],
+    },
+    {
+      numb: 3,
+      question: "Qual foi o ano do primeiro título mundial conquistado pelo São Paulo?",
+      answer: "1992",
+      options: ["1993", "1992", "2005", "2016"],
+    },
+    {
+      numb: 4,
+      question: "Quem é o maior artilheiro da história do São Paulo FC?",
+      answer: "Serginho Chulapa",
+      options: ["Serginho Chulapa", "Careca", "Luisão", "Raí"],
+    },
+    {
+      numb: 5,
+      question: "Em qual ano o São Paulo venceu o seu terceiro Mundial de Clubes?",
+      answer: "2005",
+      options: ["1999", "2000", "2010", "2005"],
+    },
+    {
+      numb: 6,
+      question: "Qual o nome do maior rival do São Paulo no futebol paulista?",
+      answer: "Corinthians",
+      options: ["Santos FC", "Corinthians", "Palmeiras", "Flamengo"],
+    },
+    {
+      numb: 7,
+      question: "Quantos títulos de Campeonato Brasileiro o São Paulo FC conquistou?",
+      answer: "6",
+      options: ["3", "5", "6", "7"],
+    },
+    {
+      numb: 8,
+      question: "Quem foi o técnico que comandou o São Paulo na conquista da Copa Libertadores em 2005?",
+      answer: "Paulo Autuori",
+      options: ["Telê Santana", "Paulo Autuori", "Jorge Sampaoli", "Cuca"],
+    },
+    {
+      numb: 9,
+      question: "Em que ano o São Paulo ganhou o seu primeiro título de Copa do Brasil?",
+      answer: "2023",
+      options: ["2023", "2020", "1999", "2000"],
+    },
+    {
+      numb: 10,
+      question: "Qual jogador do São Paulo FC é considerado um dos maiores ídolos da história do clube, conhecido por suas defesas e liderança?",
+      answer: "Rogério Ceni",
+      options: ["Gilberto Silva", "Raí", "Careca", "Rogério Ceni"],
+    },
+  ];
+  
